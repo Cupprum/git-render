c1e24303e1ea1ba9978e03b4aee9def1a0695282
Cupprum <branisa.samuel@icloud.com>
2024-11-18 04:01:42 +0700

    chore: wip on simplifying the git render script

--- git_render.py
+++ git_render.py
@@ -1,26 +1,25 @@
+import os
 import plotly.express as px
 from git import Repo
 
-repo = Repo(".")
-data = []
+# repo = Repo(".")
+repo = Repo("../unitycatalog")
+# repo = Repo("../../NN/posit")
 
-for blob in repo.tree().traverse():
-    path = blob.path
-    commits = list(repo.iter_commits(paths=path))
-    data.append({
-        'parent': path.removesuffix(f"/{blob.name}") if "/" in path else "ALL",
-        'path': path,
-        'size': blob.size,
-        'commits': len(commits) if blob.type == "blob" else 0,
-        # 'collabs': len(set(c.author.email for c in commits)), # TODO: this is not being render on the screen
-    })
+data = [{
+    'parent': os.path.dirname(blob.path) or "/",
+    'path': blob.path,
+    # 'size': blob.size,
+    'commits': len(commits := list(repo.iter_commits(paths=blob.path))),
+    # 'collabs': len(set(c.author.email for c in commits))
+} for blob in repo.tree().traverse()]
 
 px.treemap(
     data,
     names='path',
     parents='parent',
-    values='size',
-    # custom_data='collabs', # TODO: does not work
+    # values='size',
+    # hover_data=['commits', 'collabs'],
     color='commits', # TODO: can color representing number of commits update when i click around? check color settings
     color_continuous_scale='amp',
 ).show()
