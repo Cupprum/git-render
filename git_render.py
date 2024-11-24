f28e031f22eb7eb777aa07966ab638523c750161
Cupprum <branisa.samuel@icloud.com>
2024-11-25 03:12:21 +0700

    chore: cleanup script

--- git_render.py
+++ git_render.py
@@ -2,24 +2,22 @@ import os
 import plotly.express as px
 from git import Repo
 
-# repo = Repo(".")
 repo = Repo("../unitycatalog")
-# repo = Repo("../../NN/posit")
 
-data = [{
+blobs = [{
     'parent': os.path.dirname(blob.path) or "/",
     'path': blob.path,
-    # 'size': blob.size,
+    'size': blob.size,
     'commits': len(commits := list(repo.iter_commits(paths=blob.path))),
-    # 'collabs': len(set(c.author.email for c in commits))
+    'collabs': len(set(c.author.email for c in commits))
 } for blob in repo.tree().traverse()]
 
 px.treemap(
-    data,
+    blobs,
     names='path',
     parents='parent',
-    # values='size',
-    # hover_data=['commits', 'collabs'],
+    values='size',
+    hover_data=['commits', 'collabs'],
     color='commits', # TODO: can color representing number of commits update when i click around? check color settings
     color_continuous_scale='amp',
 ).show()
